fce5d83c063cad43121544cc1f21c0c71c88172f
MeyglinRosales2 <rmeyglin6o@gmail.com>
2022-02-02 23:33:33 -0600

    Se ha agregado

---
+++ prueba/main.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int Num1 = 0;
+    int Num2 = 0;
+    cout << "Suma" << endl;
+    cout << "Introduce el primer numero: " << endl;
+    cin >> Num1;
+    cout << "Introduce el segundo numero: " << endl;
+    cin >> Num2;
+    cout << "La suma es: " << Num1 + Num2 << endl;
+    cout << "Resta" << endl;
+    cout << "Introduce el primer numero: " << endl;
+    cin >> Num1;
+    cout << "Introduce el segundo numero: " << endl;
+    cin >> Num2;
+    cout << "La Resta es: " << Num1 - Num2 << endl;
+    cout << "Multiplicacion" << endl;
+    cout << "Introduce el primer numero: " << endl;
+    cin >> Num1;
+    cout << "Introduce el segundo numero: " << endl;
+    cin >> Num2;
+    cout << "La Multiplicacion es: " << Num1 * Num2 << endl;
+    cout << "Division" << endl;
+    cout << "Introduce el primer numero: " << endl;
+    cin >> Num1;
+    cout << "Introduce el segundo numero: " << endl;
+    cin >> Num2;
+    cout << "La Division es: " << Num1 / Num2 << endl;
+    return 0;
+}
